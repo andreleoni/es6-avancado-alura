e2bce361ce96cfdc6ebc6dc54be0e1275fefd932
André Luiz Leoni <andreluizleoni@gmail.com>
2017-10-19 21:57:30 -0200

    continuacao

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,6 +5,9 @@ class DateHelper {
   }
 
   static textoParaData(texto) {
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+      throw new Error('Deve estar no formato aaaa-mm-dd');
+
     return new Date(...texto.split('-').map((item, i) => (item - i % 2)));
   }
 
